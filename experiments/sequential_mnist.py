94aeca0cc03d1ac29e6e2a87ba6c78c70870acbe
Steven Morad <sm2558@cam.ac.uk>
2024-10-13 01:20:00 +0800

    sequential mnist train_one_epoch

--- experiments/sequential_mnist.py
+++ experiments/sequential_mnist.py
@@ -1,7 +1,6 @@
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-import jax_dataloader as jdl
 import optax
 import tqdm
 from datasets import load_dataset  # huggingface datasets
@@ -9,13 +8,14 @@ from datasets import load_dataset  # huggingface datasets
 from memorax.train_utils import (
     get_residual_memory_models,
     loss_classify_terminal_output,
-    model_update,
+    scan_one_epoch,
 )
 
 NUM_EPOCHS = 100
 BATCH_SIZE = 32
 SEQ_LEN = 784
 NUM_LABELS = 10
+SEED = 0
 
 
 def normalize_and_flatten(x):
@@ -25,11 +25,16 @@ def normalize_and_flatten(x):
 
 
 dataset = load_dataset("mnist")
-dataloader = jdl.DataLoader(
-    dataset["train"], backend="jax", batch_size=32, shuffle=True, drop_last=False
-)
+x = jnp.array(dataset["train"]["image"])
+y = jnp.array(dataset["train"]["label"])
 
-models = get_residual_memory_models(input=1, hidden=256, output=NUM_LABELS)
+x = normalize_and_flatten(x)
+y = jax.nn.one_hot(y, NUM_LABELS)
+# batch_index = jnp.arange(jnp.ceil(x.shape[0] / BATCH_SIZE).astype(int))
+batch_index = jnp.arange(x.shape[0], step=BATCH_SIZE)
+
+key = jax.random.key(SEED)
+models = get_residual_memory_models(input=1, hidden=256, output=NUM_LABELS, key=key)
 
 for name, model in models.items():
     lr_schedule = optax.constant_schedule(0.001)
@@ -38,19 +43,27 @@ for name, model in models.items():
         optax.adamw(lr_schedule),
     )
     opt_state = opt.init(eqx.filter(model, eqx.is_inexact_array))
+    key = jax.random.key(SEED)
 
-for epoch in range(NUM_EPOCHS):
-    pbar = tqdm.tqdm(dataloader)
-    for batch in pbar:
-        model, opt_state, loss_info = eqx.filter_jit(model_update)(
+    pbar = tqdm.tqdm(range(NUM_EPOCHS))
+    for epoch in pbar:
+        key, shuffle_key = jax.random.split(key)
+        shuffle_idx = jax.random.permutation(shuffle_key, x.shape[0])
+        x = x[shuffle_idx]
+        y = y[shuffle_idx]
+        # Create batches
+        # x = batch(x, BATCH_SIZE)
+        # y = batch(y, BATCH_SIZE)
+        model, opt_state, loss_info = scan_one_epoch(
             model=model,
             opt=opt,
             opt_state=opt_state,
             loss_fn=loss_classify_terminal_output,
-            x=batch["image"],
-            y=batch["label"],
-            x_transform=normalize_and_flatten,
-            y_transform=lambda y: jax.nn.one_hot(y, NUM_LABELS),
+            xs=x,
+            ys=y,
+            batch_size=BATCH_SIZE,
+            batch_index=batch_index,
+            key=key,
         )
         pbar.set_description(
             f"{name} epoch: {epoch}, "
--- memorax/train_utils.py
+++ memorax/train_utils.py
@@ -1,8 +1,12 @@
+from typing import Callable, Dict, Tuple
+
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 import optax
+from jaxtyping import Array, Shaped
 
+import memorax
 from memorax.magmas.elman import Elman
 from memorax.magmas.gru import GRU
 from memorax.magmas.mgu import MGU
@@ -17,7 +21,9 @@ from memorax.monoids.lru import LRU
 from memorax.monoids.mlstm import MLSTM
 
 
-def cross_entropy(y_hat, y):
+def cross_entropy(
+    y_hat: Shaped[Array, "Batch ..."], y: Shaped[Array, "Batch ..."]
+) -> Shaped[Array, "1"]:
     return -jnp.mean(jnp.sum(y * jax.nn.log_softmax(y_hat, axis=-1), axis=-1))
 
 
@@ -25,7 +31,11 @@ def accuracy(y_hat, y):
     return jnp.mean(jnp.argmax(y, axis=-1) == jnp.argmax(y_hat, axis=-1))
 
 
-def loss_classify_terminal_output(model, x, y):
+def loss_classify_terminal_output(
+    model: memorax.groups.Module,
+    x: Shaped[Array, "Batch Time Feature"],
+    y: Shaped[Array, "Batch Feature"],
+) -> Tuple[Shaped[Array, "1"], Dict[str, Array]]:
     """Given a sequence of inputs x1, ..., xn and predicted outputs y1p, ..., y1n,
     return the cross entropy loss between the true yn and predicted y1n.
 
@@ -58,18 +68,16 @@ def loss_classify_terminal_output(model, x, y):
     return loss, {"loss": loss, "accuracy": acc}
 
 
-def model_update(
-    model,
-    opt,
-    opt_state,
-    loss_fn,
-    x,
-    y,
-    x_transform=lambda x: x,
-    y_transform=lambda y: y,
-):
-    x = x_transform(x)
-    y = y_transform(y)
+def update_model(
+    model: memorax.groups.Module,
+    loss_fn: Callable,
+    opt: optax.GradientTransformation,
+    opt_state: optax.OptState,
+    x: Shaped[Array, "Batch ..."],
+    y: Shaped[Array, "Batch ..."],
+    key=None,
+) -> Tuple[memorax.groups.Module, optax.OptState, Dict[str, Array]]:
+    """Update the model using the given loss function and optimizer."""
     grads, loss_info = eqx.filter_grad(loss_fn, has_aux=True)(model, x, y)
     updates, opt_state = opt.update(
         grads, opt_state, params=eqx.filter(model, eqx.is_inexact_array)
@@ -78,9 +86,60 @@ def model_update(
     return model, opt_state, loss_info
 
 
+@eqx.filter_jit
+def scan_one_epoch(
+    model: memorax.groups.Module,
+    opt: optax.GradientTransformation,
+    opt_state: optax.OptState,
+    loss_fn: Callable,
+    xs: Shaped[Array, "Datapoint ..."],
+    ys: Shaped[Array, "Datapoint ..."],
+    batch_size: int,
+    batch_index: Shaped[Array, "Batch ..."],
+    *,
+    key: jax.random.PRNGKey,
+) -> Tuple[memorax.groups.Module, optax.OptState, Dict[str, Array]]:
+    """Train a single epoch using the scan operator. Functions as a dataloader and train loop."""
+    assert (
+        xs.shape[0] == ys.shape[0]
+    ), f"batch size mismatch: {xs.shape[0]} != {ys.shape[0]}"
+    params, static = eqx.partition(model, eqx.is_array)
+
+    def get_batch(x, y, step):
+        """Returns a specific batch of size `batch_size` from `x` and `y`."""
+        start = step * batch_size
+        x_batch = jax.lax.dynamic_slice_in_dim(x, start, batch_size, 0)
+        y_batch = jax.lax.dynamic_slice_in_dim(y, start, batch_size, 0)
+        return x_batch, y_batch
+
+    def inner(carry, index):
+        params, opt_state, key = carry
+        x, y = get_batch(xs, ys, index)
+        key = jax.random.split(key)[0]
+        model = eqx.combine(params, static)
+        params, opt_state, metrics = update_model(
+            model, loss_fn, opt, opt_state, x, y, key=key
+        )
+        params, _ = eqx.partition(params, eqx.is_array)
+        return (params, opt_state, key), metrics
+
+    (params, opt_state, key), epoch_metrics = jax.lax.scan(
+        inner,
+        (params, opt_state, key),
+        batch_index,
+    )
+    model = eqx.combine(params, static)
+    return model, opt_state, epoch_metrics
+
+
 def get_residual_memory_models(
-    input: int, hidden: int, output: int, num_layers: int = 2
-):
+    input: int,
+    hidden: int,
+    output: int,
+    num_layers: int = 2,
+    *,
+    key: jax.random.PRNGKey,
+) -> Dict[str, memorax.groups.Module]:
     layers = {
         # monoids
         "ffm": lambda recurrent_size, key: FFM(
@@ -130,7 +189,7 @@ def get_residual_memory_models(
             recurrent_size=hidden,
             output_size=output,
             num_layers=num_layers,
-            key=jax.random.PRNGKey(0),
+            key=key,
         )
         for name, fn in layers.items()
     }
